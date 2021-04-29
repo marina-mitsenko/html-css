77323fcf583ffdc4268f2e275e03ab3896c829f5
marina-mitsenko <79161507+marina-mitsenko@users.noreply.github.com>
2021-04-29 10:15:04 +0300

    Add files via upload

--- 23-24__HW_Form-restoran_JS_27.04.2021/js/main.js
+++ 23-24__HW_Form-restoran_JS_27.04.2021/js/main.js
@@ -11,6 +11,81 @@ let fieldAge = formMain.age;
 let fieldMenu = formMain.menu;
 let fieldOffers = formMain.offers;
 let btnSend = formMain.send;
+
+let btn1 = formMain.btn1;
+let chk1 = formMain.chk1;
+let clickBtn1 = false;
+
+let btn2 = formMain.btn2;
+let chk2 = formMain.chk2;
+let clickBtn2 = false;
+
+let btn3 = formMain.btn3;
+let chk3 = formMain.chk3;
+let clickBtn3 = false;
+
+let yes = formMain.yes;
+let no = formMain.no;
+let clickYes = false;
+let clickNo = false;
+
+yes.onclick = function(){
+        console.log(clickYes);
+        if(clickYes===false){
+                yes.setAttribute('checked', '');
+                clickYes = true;
+                no.removeAttribute('checked');
+        }else{
+                yes.removeAttribute('checked');
+                clickYes = false;
+        }
+}
+        
+no.onclick = function(){
+        console.log(clickNo);
+        if(clickNo===false){
+                no.setAttribute('checked', '');
+                clickNo = true;
+                yes.removeAttribute('checked');
+        } else {
+                no.removeAttribute('checked');
+                clickNo = false;
+        }
+        
+}
+
+
+btn1.onclick = function(){  
+        console.log(clickBtn1);
+        if(clickBtn1 === false){
+                chk1.setAttribute('checked', '');
+                clickBtn1 = true;
+        } else {
+                chk1.removeAttribute('checked');
+                clickBtn1 = false;
+        }
+};
+btn2.onclick = function(){  
+        console.log(clickBtn2);
+        if(clickBtn2 === false){
+                chk2.setAttribute('checked', '');
+                clickBtn2 = true;
+        } else {
+                chk2.removeAttribute('checked');
+                clickBtn2 = false;
+        }
+};
+btn3.onclick = function(){  
+        console.log(clickBtn3);
+        if(clickBtn3 === false){
+                chk3.setAttribute('checked', '');
+                clickBtn3 = true;
+        } else {
+                chk3.removeAttribute('checked');
+                clickBtn3 = false;
+        }
+};
+
 console.log(btnSend);
 fieldName.onblur = function(){
         let value = this.value;
@@ -22,6 +97,8 @@ fieldName.onblur = function(){
                 console.log('Елемент не может быть пустым');
                 this.focus();
                 fieldTelefon.setAttribute('disabled', '');
+                btnSend.setAttribute('disabled', '');
+              //  fieldName.setPlaceholder('Елемент не может быть пустым', '');
         }
 }
 fieldTelefon.onblur = function(){
@@ -34,6 +111,7 @@ fieldTelefon.onblur = function(){
                 console.log('Елемент не может быть пустым');
                 this.focus();
                 fieldEmail.setAttribute('disabled', '');
+                btnSend.setAttribute('disabled', '');
         }
 }
 fieldEmail.onblur = function(){
@@ -46,6 +124,7 @@ fieldEmail.onblur = function(){
                 console.log('Елемент не может быть пустым');
                 this.focus();
                 fieldData.setAttribute('disabled', '');
+                btnSend.setAttribute('disabled', '');
         }
 }
 fieldData.onblur = function(){
@@ -58,6 +137,7 @@ fieldData.onblur = function(){
                 console.log('Елемент не может быть пустым');
                 this.focus();
                 fieldAge.setAttribute('disabled', '');
+                btnSend.setAttribute('disabled', '');
         }
 }
 fieldAge.onblur = function(){
@@ -70,6 +150,7 @@ fieldAge.onblur = function(){
                 console.log('Елемент не может быть пустым');
                 this.focus();
                 fieldMenu.setAttribute('disabled', '');
+                btnSend.setAttribute('disabled', '');
         }
 }
 fieldMenu.onblur = function(){
@@ -82,6 +163,7 @@ fieldMenu.onblur = function(){
                 console.log('Елемент не может быть пустым');
                 this.focus();
                 fieldOffers.setAttribute('disabled', '');
+                btnSend.setAttribute('disabled', '');
         }
 }
 fieldOffers.onblur = function(){
