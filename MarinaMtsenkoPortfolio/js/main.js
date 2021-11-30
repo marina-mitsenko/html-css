36911e3c03e51b156bb344da529a17b5db1215ff
marina-mitsenko <79161507+marina-mitsenko@users.noreply.github.com>
2021-11-30 16:28:36 +0200

    Add files via upload

--- MarinaMtsenkoPortfolio/js/main.js
+++ MarinaMtsenkoPortfolio/js/main.js
@@ -4,9 +4,16 @@ let close = doc.querySelector('.menu__close');
 let menu = doc.querySelector('.menu');
 let toggle = doc.querySelector('.header__toggle-menu');
 let write = doc.querySelector('.write');
+let call = doc.querySelector('.call');
 let rapport = doc.querySelector('.rapport');
 let formButton = doc.querySelector('.form__button');
 console.log(write);
+let showWrite = setTimeout(function() {
+    write.style.display="block";
+  }, 10000);
+let showCall = setTimeout(function() {
+    call.style.display="block";
+  }, 5000);
 close.onclick = function(){
     menu.style.zIndex="-1";
 }
